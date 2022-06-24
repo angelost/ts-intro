3c80d5d72b89c542f8f20481a93cce98e6255b25
Angelo Gonzalez <angelo.gonzalez@live.com>
2022-06-24 13:23:03 -0400

    25. Extender una clase

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
 /*
     ===== Código de TypeScript =====
 */
-
-class Heroe {
-    // alterEgo: string;
-    // edad: number;
-    // nombreReal: string;
-
-    constructor(
-        public alterEgo: string,
-        public edad: number,
-        public nombreReal: string
-    ) {}
-}
-
-const ironman = new Heroe('Ironman', 45, 'Tony');
-
-console.log(ironman);
